390c885e52b8d1584f338888e238b88be3a0ae8d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-04 21:39:21 +0000

    Add application.rb with RCat module inside.

---
+++ lib/rcat/application.rb
@@ -0,0 +1,22 @@
+module RCat
+  class Application
+    def initialize(argv)
+      @params, @files = parse_options(argv)
+      @display = RCat::Display.new(@params)
+    end
+
+    def run
+      if @files.empty?
+        @display.render(STDIN)
+      else
+        @files.each do |filename|
+          File.open(filename) { |f| @display.render(f) }
+        end
+      end
+    end
+
+    def parse_options(argv)
+      # ignore for now
+    end
+  end
+end
